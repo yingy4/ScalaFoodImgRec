49918519378284d06f3222fc7d30ab19e248ef4a
Fei.Shen <imshenfei@gmail.com>
2017-12-13 13:40:09 -0500

    added test input source file in rest module

--- core/src/test/scala/SparkTest/CountUserSpec.scala
+++ core/src/test/scala/SparkTest/CountUserSpec.scala
@@ -1,26 +1,32 @@
 package SparkTest
 
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 import tensorflow.sparkBulk.WordCount
 
 class CountUserSpec extends FlatSpec with Matchers with BeforeAndAfter  {
 
-  private var sc: SparkContext = _
+  implicit var spark: SparkSession = _
 
   before {
-    sc = new SparkContext(new SparkConf().setAppName("WordCount").setMaster("local[*]"))
+    spark = SparkSession
+      .builder()
+      .appName("WordCount")
+      .master("local[*]")
+      .getOrCreate()
   }
 
   after {
-    if (sc != null) {
-      sc.stop()
+    if (spark != null) {
+      spark.stop()
     }
   }
 
   "result" should "right for wordCount" in {
-    WordCount.wordCount(sc.textFile("input//test//testfile2.txt")," ").collect() should matchPattern {
-      case Array(("sushi",2),("pizza",2)) =>
+    WordCount.wordCount(spark.read.textFile("input//test//testfile2.txt").rdd," ").collect() should matchPattern {
+      case Array(("sushi",2), ("pizza",2)) =>
     }
   }
+
 }
